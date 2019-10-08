56f4a6c60e726bc121959eece10aea546241ccc9
strangehood <55440847+strangehood@users.noreply.github.com>
2019-10-08 12:09:17 +0300

    star_animation() animate shooting stars with tails

--- shooting stars.py
+++ shooting stars.py
@@ -8,10 +8,14 @@ canvasSize(500, 500)
 xspeed = 0
 yspeed = 0
 star = None
+x0 = 0
+y0 = 0
+x1 = 0
+y1 = 0
 
 
 def star_generation():  # func generates random star
-    global xspeed, yspeed, star
+    global xspeed, yspeed, star, x0, y0
 
     # color generation:
     red = r.randint(155, 230)
@@ -26,24 +30,41 @@ def star_generation():  # func generates random star
 
     # star size and speed generation:
     rad = r.randint(5, 10)
-    xspeed = r.randint(4, 8) * r.choice([-1, 1])
-    yspeed = r.randint(4, 8) * r.choice([-1, 1])
+    xspeed = r.randint(8, 12) * r.choice([-1, 1])
+    yspeed = r.randint(4, 6) * r.choice([-1, 1])
 
     # star drawing
     if (xspeed > 0) and (yspeed > 0):
-        star = circle(0, 0, rad)
+        x0 = 0
+        y0 = 0
+        star = circle(y0, x0, rad)
     elif (xspeed < 0) and (yspeed > 0):
-        star = circle(500, 0, rad)
-    elif xspeed > 0:
+        x0 = 500
+        y0 = 0
+        star = circle(x0, y0, rad)
+    elif (xspeed > 0) and (yspeed < 0):
+        x0 = 0
+        y0 = 500
         star = circle(0, 500, rad)
     else:
+        x0 = 500
+        y0 = 500
         star = circle(500, 500, rad)
 
 
 def starfall():  # func moves random star
+    global x1, y1
+    star_tail = None
+    x1 += xspeed
+    y1 += yspeed
+    deleteObject(star_tail)
+    star_tail = line(x0, y0, x1, y1)
     moveObjectBy(star, xspeed, yspeed)
 
 
+
 star_generation()
+x1 = x0
+y1 = y0
 onTimer(starfall, 100)
 run()
